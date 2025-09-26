6bc1ca699342ecd8741f4ef587c6799b8194cf44
faizkhan77 <nurfarzanakhan7@gmail.com>
2025-09-26 10:44:47 +0530

    fixed double inner shadow border in typing bubble

--- src/components/chat/TypingAnimation.tsx
+++ src/components/chat/TypingAnimation.tsx
@@ -29,23 +29,25 @@ export default function TypingAnimation() {
 
   return (
     <div className="bg-accent/20 text-sm border max-w-xl px-2 flex w-fit items-center gap-2 rounded-lg">
-    <div className="flex items-end space-x-3 px-2 py-2 rounded-md shadow-inner">
-      <span className="text-sm font-medium text-text-secondary">Analyzing</span>
-      <motion.div
-        className="flex gap-1"
-        variants={containerVariants}
-        initial="initial"
-        animate={controls}
-      >
-        {[...Array(3)].map((_, index) => (
-          <motion.div
-            key={index}
-            className="h-1.5 w-1.5 mb-1 rounded-full bg-[#0dd3ff] shadow-md opacity-65"
-            variants={dotVariants}
-          />
-        ))}
-      </motion.div>
+      <div className="flex items-end space-x-3 px-2 py-2 rounded-md">
+        <span className="text-sm font-medium text-text-secondary">
+          Analyzing
+        </span>
+        <motion.div
+          className="flex gap-1"
+          variants={containerVariants}
+          initial="initial"
+          animate={controls}
+        >
+          {[...Array(3)].map((_, index) => (
+            <motion.div
+              key={index}
+              className="h-1.5 w-1.5 mb-1 rounded-full bg-[#0dd3ff] shadow-md opacity-65"
+              variants={dotVariants}
+            />
+          ))}
+        </motion.div>
+      </div>
     </div>
-        </div>
   );
 }
